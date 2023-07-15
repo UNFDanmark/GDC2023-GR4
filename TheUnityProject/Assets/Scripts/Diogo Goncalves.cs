299ff3f8a2f653967a0ba523b44a70bfd9d64f7e
Deltager <teknisk+GDC2023-GR4@game.unf.dk>
2023-07-15 21:10:07 +0200

    Beam
    we use raycast now it knows whats an enemy and a wall yippie

--- TheUnityProject/Assets/Scripts/Diogo Goncalves.cs
+++ TheUnityProject/Assets/Scripts/Diogo Goncalves.cs
@@ -22,7 +22,8 @@ public class DiogoGonclaves : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
