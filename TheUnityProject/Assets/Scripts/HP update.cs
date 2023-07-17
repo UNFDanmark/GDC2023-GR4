041c71fe5d4aa684f7b9b390dcec22f31d5415a2
Deltager <teknisk+GDC2023-GR4@game.unf.dk>
2023-07-17 12:01:51 +0200

    cursor

---
+++ TheUnityProject/Assets/Scripts/HP update.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPupdate : MonoBehaviour
+{
+    public int hp;
+    
+    public 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
