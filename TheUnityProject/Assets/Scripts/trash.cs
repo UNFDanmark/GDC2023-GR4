32f8c0fe590dab882590314746dcaae9d681acdd
Deltager <teknisk+GDC2023-GR4@game.unf.dk>
2023-07-18 10:02:52 +0200

    new spawn function i try

--- TheUnityProject/Assets/Scripts/trash.cs
+++ TheUnityProject/Assets/Scripts/trash.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
-public class enemyspawner : MonoBehaviour
+public class trash : MonoBehaviour
 {
-    
     [SerializeField] private float cooldown;
     [SerializeField] private int maxamount;
     [SerializeField] private Transform spawnArea;
@@ -22,50 +20,41 @@ public class enemyspawner : MonoBehaviour
     [SerializeField] private Transform player;
 
     private int timeLeft;
-    float wait = 1.5;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CallSpawner());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
     }
-
-    public IEnumerator CallSpawner()
-    {
-        while (true)
-        {
-            Debug.Log("Started at timestamp: " + Time.time);
-            yield return new WaitForSeconds(wait);
-            spawner();
-            Debug.Log("Finished at timestamp: " + Time.time);
-        }
-        
-    }
-    
     Vector3 PositionRandomizer()
     {
         float x = Random.Range(-spawnArea.localScale.x / 2, spawnArea.localScale.x / 2);
+
         float y = Random.Range(-spawnArea.localScale.y / 2, spawnArea.localScale.y / 2);
+        
         return new Vector3(x, 0.75f, y);
     }
-    
+
     private void spawner()
     {
-
-        GameObject newEnemy = Instantiate(Enemy,PositionRandomizer(),Enemy.transform.rotation);
-        enemy enemyscript = newEnemy.GetComponent<enemy>();
-        enemyscript.player = player;
-        currentAmountOfEnemies++;
-
+        timeLeftBetweenSpawns -= Time.deltaTime;
+        if (timeLeftBetweenSpawns <= 0 && currentAmountOfEnemies < maxAmountOfEnemies)
+        {
+            
+            GameObject newEnemy = Instantiate(Enemy,PositionRandomizer(),Enemy.transform.rotation);
+            enemy enemyscript = newEnemy.GetComponent<enemy>();
+            enemyscript.player = player;
+            
+  
+            
+            timeLeftBetweenSpawns = spawnCooldown;
+            currentAmountOfEnemies ++;
+        }
     }
 }
-    
-
-
